36f4deb6283e564ef862de77e334c9d1ae62a9e5
Jessica Grzanna <jgrzanna@me.com>
2019-07-11 19:25:36 +0200

    feat: Adds deck details to Deck view
    - Shows number of cards in the deck and the deck title
    - Has two link-buttons to either start a quiz for this deck or add a new card to the deck

--- components/Deck.js
+++ components/Deck.js
@@ -1,13 +1,41 @@
-import React from "react";
-import { Text, View, Platform, StyleSheet } from "react-native";
-import { white, rose } from "../utils/colors";
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Platform,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import { black, white, creme, pink, gold, gray } from "../utils/colors";
 
-export default function Deck(props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.h2}>Deck View</Text>
-    </View>
-  );
+export default class Deck extends Component {
+  render() {
+    const { navigation } = this.props;
+    const { deck } = navigation.state.params;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.h2}>Deck View</Text>
+        <View style={styles.deck}>
+          <Text style={styles.p}>{deck.title}</Text>
+          <Text style={styles.p}>{deck.questions.length} cards</Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={[styles.button, { borderColor: pink, borderWidth: 1 }]}
+            onPress={() => navigation.navigate("Quiz", { deck, cardIndex: 0 })}
+          >
+            <Text style={{ color: pink }}>Start Quiz</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: pink }]}
+            onPress={() => navigation.navigate("NewCard", { deck })}
+          >
+            <Text style={{ color: white }}>Add Card</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,6 +46,27 @@ const styles = StyleSheet.create({
   },
   h2: {
     fontSize: 24,
-    margin: 20
+    margin: 20,
+    color: pink
+  },
+  deck: {
+    backgroundColor: creme,
+    borderRadius: Platform.OS === "ios" ? 14 : 2,
+    paddingTop: 50,
+    paddingBottom: 50,
+    paddingLeft: 80,
+    paddingRight: 80
+  },
+  p: {
+    color: gray,
+    fontSize: 18
+  },
+  button: {
+    borderRadius: Platform.OS === "ios" ? 14 : 2,
+    paddingTop: 30,
+    paddingBottom: 30,
+    paddingLeft: 60,
+    paddingRight: 60,
+    marginBottom: 15
   }
 });
